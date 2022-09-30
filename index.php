796e4b3f3f60da83e16c177b23895054e8c0de76
Konstantin-Kozlov <konstantin.kozlov@novapulsar.com>
2022-09-30 22:36:52 +0300

    new version

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
 </head>
 <body>
 <form action="" method="post">
+
     <!-- поле ввода первого числа -->
     <label for="num1">First number:</label><br>
     <input type="text" id="num1" name="num1" required/><br>
 
     <!-- блок с кнопками -->
     <div>
-        <button name="myActionName" type="submit" value="0">0</button>
-        <button name="myActionName" type="submit" value="1">1</button>
-        <button name="myActionName" type="submit" value="2">2</button>
-        <button name="myActionName" type="submit" value="3">3</button>
+        <input type="radio" name="oper" value="plus" />+ <br>
+        <input type="radio" name="oper" value="minus" />- <br>
+        <input type="radio" name="oper" value="mult" />* <br>
+        <input type="radio" name="oper" value="div" />/ <br>
     </div>
 
     <!-- поле ввода второго числа -->
@@ -26,59 +27,13 @@
     <button type="submit" name="submit">Посчитать</button>
 
     <p id="result"></p>
-
 </form>
 </body>
 </html>
 
-
 <?php
-
-require 'CalculatorView.php';
-require 'Calculator.php';
-require 'CalculatorPresenter.php';
-
-$view = new CalculatorView();
-$model = new Calculator();
-$presenter = new CalculatorPresenter();
-$presenter->setModel($model);
-$presenter->setView($view);
-
-if(isset($_POST['submit'])) {
-    $view->setFirstArgument($_POST['num1']);
-    $view->getFirstArgumentAsString();
-    $view->setSecondArgument($_POST['num2']);
-    print_r($_POST);
-    if(isset($_POST['submit'])) {
-        if (isset($_POST['myActionName'])) {
-            switch ($_POST['myActionName']) {
-                case '0':
-                    echo 1;
-                    break;
-                case '1':
-                    include_once 'you_file2.php';
-                    break;
-                case '2':
-                    include_once 'you_file3.php';
-                    break;
-                case '3':
-                    include_once 'you_file4.php';
-                    break;
-            }
-        }
-    }
-//    if($_POST['operation'] == 'minus')
-//    {
-//        $total = $_POST['number1'] - $_POST['number2'];
-//    }
-//    if($_POST['operation'] == 'times')
-//    {
-//        $total = $_POST['number1'] * $_POST['number2'];
-//    }
-//    if($_POST['operation'] == 'divided by')
-//    {
-//        $total = $_POST['number1'] / $_POST['number2'];
-//    }
-
-}
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+$oper = $_POST["oper"];
+echo $num1 . " " . $num2 . " " . $oper;
 ?>
