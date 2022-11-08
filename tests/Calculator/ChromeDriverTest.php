dd52e2d6ab11f09641adad42922b9457e33f8cbd
Pavel Bogachev <boga4ev13@mail.ru>
2022-11-08 08:29:33 +0300

    feat(selenium): added tests covering all buttons

--- tests/Calculator/ChromeDriverTest.php
+++ tests/Calculator/ChromeDriverTest.php
@@ -44,6 +44,75 @@ class ChromeDriverTest extends TestCase
             $element->sendKeys("2");
             sleep(1);
         }
+        $element = $this->driver->findElement(WebDriverBy::id("/"));
+        if($element) {
+            $element->click();
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::name("result"));
+        $this->assertEquals(0.5, $element->getText());
+    }
+
+    public function testPlus()
+    {
+        $this->driver->get("http://calculator/");
+        $this->driver->manage()->window()->maximize();
+        $element = $this->driver->findElement(WebDriverBy::name("num1"));
+        if($element) {
+            $element->sendKeys("1");
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::name("num2"));
+        if($element) {
+            $element->sendKeys("2");
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::id("+"));
+        if($element) {
+            $element->click();
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::name("result"));
+        $this->assertEquals(3, $element->getText());
+    }
+
+    public function testMultiply()
+    {
+        $this->driver->get("http://calculator/");
+        $this->driver->manage()->window()->maximize();
+        $element = $this->driver->findElement(WebDriverBy::name("num1"));
+        if($element) {
+            $element->sendKeys("1");
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::name("num2"));
+        if($element) {
+            $element->sendKeys("2");
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::id("*"));
+        if($element) {
+            $element->click();
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::name("result"));
+        $this->assertEquals(2, $element->getText());
+    }
+
+    public function testMinus()
+    {
+        $this->driver->get("http://calculator/");
+        $this->driver->manage()->window()->maximize();
+        $element = $this->driver->findElement(WebDriverBy::name("num1"));
+        if($element) {
+            $element->sendKeys("1");
+            sleep(1);
+        }
+        $element = $this->driver->findElement(WebDriverBy::name("num2"));
+        if($element) {
+            $element->sendKeys("2");
+            sleep(1);
+        }
         $element = $this->driver->findElement(WebDriverBy::id("-"));
         if($element) {
             $element->click();
